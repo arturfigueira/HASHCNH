5757d182839e6e3258fdc9f9e3872adda6b3b683
TANT-A01\sanar03 <artur.santana@ca.com>
2018-12-21 12:59:42 -0200

    Missing code to return the masked value

--- src/main/java/com/ca/datamasker/custom/HashCNH.java
+++ src/main/java/com/ca/datamasker/custom/HashCNH.java
@@ -1,5 +1,6 @@
 package com.ca.datamasker.custom;
 
+import com.grid_tools.products.datamasker.Datamasker;
 import com.grid_tools.products.datamasker.IMaskFunction;
 import com.grid_tools.products.datamasker.randfunctions;
 
@@ -7,7 +8,22 @@ public class HashCNH implements IMaskFunction {
 
     @Override
     public Object mask(Object... objects) {
-        return null;
+        String maskedValue = null;
+
+        final String inputValue = (String) objects[0];
+        if(inputValue == null || inputValue.trim().isEmpty()){
+            maskedValue = inputValue;
+        }
+
+        try{
+            maskedValue = hashCNH(inputValue);
+        }catch (IllegalArgumentException e){
+            Datamasker.processOutputs(Datamasker.formatMessage("m0345-hasherr", new String[] { "HASH" }));
+            Datamasker.processErrors(Datamasker.formatMessage("m0345-hasherr", new String[] { "HASH" }));
+            Datamasker.processOutputs(Datamasker.formatMessage("m0155-DBValue", new String[] { inputValue }));
+            System.exit(1);
+        }
+        return maskedValue;
     }
 
     protected String hashCNH(final String cnh){
